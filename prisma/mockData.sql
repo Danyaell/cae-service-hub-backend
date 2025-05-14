21387978e9db0f79cd88c2bba579a8b6927f6e05
Danyaell <danyaellmtzo17@gmail.com>
2025-05-14 13:48:41 -0600

    migrate-db

---
+++ prisma/mockData.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM cae_service_hub_db.reports
+LIMIT 0, 1000
+
+-- Date: 2025-05-14 13:44
+*/
+INSERT INTO `` (`id`,`report_date`,`reporter_name`,`role`,`room`,`pc`,`description`,`attendant`,`action_taken`) VALUES (1,'2025-05-12','Danyaell','Encargado','203','10','No enciende','Bryan','Se prendió');
+/*
+-- Query: SELECT * FROM cae_service_hub_db.software_requests
+LIMIT 0, 1000
+
+-- Date: 2025-05-14 13:47
+*/
+INSERT INTO `` (`id`,`request_date`,`requestor_name`,`room`,`software`,`attendant`,`commitment_date`) VALUES (1,'2025-05-14','Jorge Iván','204','Android Studio','Danyaell','2025-05-20');
