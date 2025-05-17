9187645cda7731e134fa716497f229eccdc25962
Danyaell <danyaellmtzo17@gmail.com>
2025-05-17 00:42:43 -0600

    config-cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import softwareRequestsRouter from './routes/softwareRequests.routes';
 import reportsRouter from './routes/reports.routes';
 import { API_REPORTS, API_SOFTWARE_REQUEST } from './constants/routes.const';
 
 const PORT = 3000;
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
