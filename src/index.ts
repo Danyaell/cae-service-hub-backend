1115e74fe160d01e02c29c5dff0f143982ba8446
Danyaell <danyaellmtzo17@gmail.com>
2025-05-20 07:12:41 -0600

    just-blank-space

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
 }));
+
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
