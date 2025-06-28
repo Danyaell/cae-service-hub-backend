873361f985e8301ea3c6ca1901fae67f44eacdb8
Danyaell <danyaellmtzo17@gmail.com>
2025-06-28 11:10:19 -0600

    update-endpoint-users

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 import { deleteUser, getAllUsers, login, signin, updateUser } from '../controllers/users.controller';
-import { authMiddleware } from '../utils/auth';
 
 const router = express.Router();
 
-router.get('/', authMiddleware, getAllUsers);
+router.get('/', getAllUsers);
 router.post('/register', signin);
 router.post('/login', login);
 router.put('/:id', updateUser);
