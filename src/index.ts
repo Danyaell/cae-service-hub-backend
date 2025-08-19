fb5e07ce800eeecb7c1840cb8da9efb7069167db
Danos <danyaellmtzo17@gmail.com>
2025-08-18 19:52:05 -0600

    create-lost-items-service

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import cors from 'cors';
 import usersRouter from './routes/users.routes';
 import softwareRequestsRouter from './routes/softwareRequests.routes';
 import reportsRouter from './routes/reports.routes';
-import { API_REPORTS, API_SOFTWARE_REQUEST, API_USERS } from './constants/routes.const';
+import lostItemsRouter from './routes/lostItems.routes';
+import { API_LOST_ITEMS, API_REPORTS, API_SOFTWARE_REQUEST, API_USERS } from './constants/routes.const';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -22,4 +23,5 @@ app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 app.use(API_SOFTWARE_REQUEST, softwareRequestsRouter);
 app.use(API_REPORTS, reportsRouter);
+app.use(API_LOST_ITEMS, lostItemsRouter);
 app.use(API_USERS, usersRouter);
